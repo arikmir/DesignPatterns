2e7f9a9585d8b8c249ff7769e2222aa88cb45929
Arik Mir <mirarikintenam@gmail.com>
2022-03-29 10:38:28 +1000

    Singleton

---
+++ DesignPatterns/DesignPatterns/Creational Design Patterns/SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational_Design_Patterns.SingletonDesignPattern
+{
+    //https://dotnettutorials.net/lesson/singleton-design-pattern/
+    public sealed class Singleton
+    {
+        private static Singleton instance =null;
+        private static int counter = 0;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -8,29 +8,18 @@ namespace DesignPatterns
 {
     using DesignPatterns.Creational_Design_Patterns.BuilderDesignPatternCoffee;
     using DesignPatterns.Creational_Design_Patterns.FluidInterfaceDesign_Pattern;
+    using DesignPatterns.Creational_Design_Patterns.SingletonDesignPattern;
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            FluentEmployee employee = new FluentEmployee();
+            Singleton fromTeacher = Singleton.GetInstance;
+            fromTeacher.PrintDetails("From Teacher");
+            Singleton fromStundet = Singleton.GetInstance;
+            fromStundet.PrintDetails("From Studnet");
 
-            employee.EmployeeName("Arik").BornDate("10/10/1992").WorkingOn("IT").StaysAt("Mumbai-India");
-
-            Employee employee1 = new Employee();
-            employee1.FullName = "Anurag";
-            employee1.Department = "IT";
-
-            Employee employee2 = employee1.GetClone();
-            employee2.FullName = "Pranaya";
-            employee2.Department = "tet";
-
-            Console.WriteLine("Emplpyee 1: ");
-            Console.WriteLine("Name: " + employee1.FullName + ", Department: " + employee1.Department);
-            Console.WriteLine("Emplpyee 2: ");
-            Console.WriteLine("Name: " + employee2.FullName + ", Department: " + employee2.Department);
-            Console.Read();
-
+            Console.ReadLine();
         }
     }
 }
