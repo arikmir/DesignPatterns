f71857a9659e6ddb9a5abbcb60f0ce26468d9cb9
Arik Mir <mirarikintenam@gmail.com>
2022-04-20 11:30:03 +1000

    TemplateDesign

---
+++ DesignPatterns/DesignPatterns/Behavioral Design Patterns/TemplateDesignPattern/HouseTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral_Design_Patterns.TemplateDesignPattern
+{
+    public abstract class HouseTemplate
+    {
+        public void BuildHouse()
+        {
+            this.BuildFoundation();
+            this.BuildPillars();
+            this.BuildWalls();
+            this.BuildWindows();
+            Console.WriteLine("House is built");
+        }
+
+        protected abstract void BuildFoundation();
+        protected abstract void BuildPillars();
+        protected abstract void BuildWalls();
+        protected abstract void BuildWindows();
+
+
+    }
+}
--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -24,30 +24,24 @@ using DesignPatterns.Behavioral_Design_Patterns.ChainOfResponsibility;
 using DesignPatterns.Behavioral_Design_Patterns.RTChainOfResponsibility;
 using DesignPatterns.Behavioral_Design_Patterns.StateDesignPattern;
 using DesignPatterns.Behavioral_Design_Patterns.RealTimeStateDesignPattern;
-
+using DesignPatterns.Behavioral_Design_Patterns.TemplateDesignPattern;
 
 namespace DesignPatterns
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            // Initially Vending Machine will be 'noMoneyState'
-            VendingMachine vendingMachine = new VendingMachine();
-            Console.WriteLine("Current VendingMachine State : "
-                              + vendingMachine.vendingMachineState.GetType().Name + "\n");
-            vendingMachine.DispenseProduct();
-            vendingMachine.SelectProductAndInsertMoney(50, "Pepsi");
-            // Money has been inserted so vending Machine internal state
-            // changed to 'hasMoneyState'
-            Console.WriteLine("\nCurrent VendingMachine State : "
-                              + vendingMachine.vendingMachineState.GetType().Name + "\n");
-            vendingMachine.SelectProductAndInsertMoney(50, "Fanta");
-            vendingMachine.DispenseProduct();
-            // Product has been dispensed so vending Machine internal state
-            // changed to 'NoMoneyState'
-            Console.WriteLine("\nCurrent VendingMachine State : "
-                              + vendingMachine.vendingMachineState.GetType().Name);
+            Console.WriteLine("Build a Concrete House\n");
+            HouseTemplate houseTemplate = new ConcreteHouse();
+            // call the template method
+            houseTemplate.BuildHouse();
+            Console.WriteLine();
+            Console.WriteLine("Build a Wooden House\n");
+            houseTemplate = new WoodenHouse();
+            // call the template method
+            houseTemplate.BuildHouse();
             Console.Read();
         }
     }
